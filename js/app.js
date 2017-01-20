1b170b0baecb3990c720d74cbff50e5898a01ce2
Victor <viclee02@gmail.com>
2017-01-19 15:56:20 -1000

    complete assignment

---
+++ js/app.js
@@ -0,0 +1,44 @@
+/*Use the Document Object Model (DOM) to complete the following exercises below:*/
+
+/*1. Replace the n/a with the following:
+
+Tay-Tay*/
+
+
+
+/*2. Replace the n/a with the following: 
+
+Project Manager*/
+
+
+
+/*3. Replace the n/a with the following:
+
+Concatenation*/
+
+
+
+/*4. Replace the n/a for Snoop with the gangsta lorem ipsum*/
+
+
+
+/*5. Relace the n/a for Samuel L Jackson with his lorem ipsum*/
+
+
+
+
+/*6. Create a div element for Chuck Norris and give it an id of "name7". Inside this div element, give it the contents of "Chuck Norris".
+
+Append this div element to the nameBox div*/
+
+ 
+
+/*7. Create a div element for Grimmace and give it an id of "alias8". Inside this div element, give it the contents of "The Purple Monster".
+
+Append this div element to the aliasBox div.*/
+
+
+
+
+//Final Boss
+/*9. Create your own profile.*/
